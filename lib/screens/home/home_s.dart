47ad09be9034466d5506334f55a38c08bc71ec90
rubydenial <kabalamabonomo@gmail.com>
2022-03-22 19:58:08 +0200

    time - 5.23

--- lib/screens/home/home_s.dart
+++ lib/screens/home/home_s.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_ui/constants.dart';
+import 'package:food_ui/demo_data.dart';
 
 class HomeS extends StatelessWidget {
   const HomeS({Key? key}) : super(key: key);
@@ -7,34 +8,45 @@ class HomeS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        title: Column(
-          children: [
-            Text(
-              'delivery to'.toUpperCase(),
-              style: Theme.of(context)
-                  .textTheme
-                  .caption!
-                  .copyWith(color: kActiveColor),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Column(
+              children: [
+                Text(
+                  'delivery to'.toUpperCase(),
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption!
+                      .copyWith(color: kActiveColor),
+                ),
+                const Text(
+                  'San Francisco',
+                  style: TextStyle(color: Colors.black),
+                )
+              ],
             ),
-            const Text(
-              'San Francisco',
-              style: TextStyle(color: Colors.black),
-            )
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text('Filter', style: TextStyle(color: Colors.black)),
+            actions: [
+              TextButton(
+                onPressed: () {},
+                child:
+                    const Text('Filter', style: TextStyle(color: Colors.black)),
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: AspectRatio(
+              aspectRatio: 1.81,
+              child: PageView.builder(
+                itemCount: demoBigImages.length,
+                itemBuilder: (_, index) => Image.asset(demoBigImages[index]),
+              ),
+            ),
           ),
         ],
-      ),
-      body: CustomScrollView(
-        slivers: [SliverAppBar()],
       ),
     );
   }
